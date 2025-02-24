15db51624e8cfc36f65503c6c588d6e3ed6d2dcd
ozzgurkarli <ozzgurkarli2@gmail.com>
2025-02-24 22:46:12 +0300

    Refactor CARDDETL to use existing DTOCreditCard instance and enhance SETTINGS UI with name and email input fields

--- lib/UI/S-SETTINGS/SETTINGS.dart
+++ lib/UI/S-SETTINGS/SETTINGS.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:parbank/components/UButton.dart';
 import 'package:parbank/components/UIcon.dart';
 import 'package:parbank/components/UIconButton.dart';
+import 'package:parbank/components/ULabel.dart';
 import 'package:parbank/components/UScaffold.dart';
+import 'package:parbank/components/UTextField.dart';
 import 'package:parbank/helpers/UColor.dart';
 
 class SETTINGS extends StatefulWidget {
@@ -12,6 +15,8 @@ class SETTINGS extends StatefulWidget {
 }
 
 class _SETTINGSState extends State<SETTINGS> {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return UScaffold(
@@ -23,6 +28,36 @@ class _SETTINGSState extends State<SETTINGS> {
         onPressed: () {
           Navigator.pop(context);
         },
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ULabel(
+              label: 'Name',
+              child: UTextField(
+                controller: nameController,
+                hintText: 'Enter your name',
+              ),
+            ),
+            SizedBox(height: 16),
+            ULabel(
+              label: 'Email',
+              child: UTextField(
+                controller: emailController,
+                hintText: 'Enter your email',
+              ),
+            ),
+            SizedBox(height: 16),
+            UButton(
+              onPressed: () {
+                // Save settings logic
+              },
+              child: Text('Save'),
+            ),
+          ],
+        ),
       ),
     );
   }
